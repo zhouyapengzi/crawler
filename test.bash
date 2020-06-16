e610d0470f682073ea6b4ae7cf1f73d4b7d5eff5
ZiPeng <zi_peng@encs.concordia.ca>
2020-06-16 12:26:49 -0400

    crawl bash

---
+++ test.bash
@@ -0,0 +1,25 @@
+python jenkins.py https://builds.apache.org/view/H-L/view/Hive/ Hive-linux-ARM-trunk hive
+
+
+python jenkins.py https://builds.apache.org/view/C/view/Apache%20CXF CXF-Master-JDK15 CXF
+
+
+python jenkins.py https://builds.apache.org/view/H-L/view/Hadoop Hadoop-qbt-linux-ARM-trunk hadoop
+
+
+python jenkins.py https://builds.apache.org/view/H-L/view/HBase HBase-Nightly-ARM hbase
+
+python jenkins.py https://builds.apache.org/view/B/view/BookKeeper bookkeeper_postcommit_master_java8 bookkeeper
+
+python jenkins.py https://builds.apache.org/ kafka-trunk-jdk11 kafka
+
+python jenkins.py https://jenkins.webtide.net/job/nightlies/ jetty-10.0.x-windows-nightly jetty
+
+python jenkins.py https://ci.eclipse.org/californium/ californium-master-nightly californium
+
+python travis.py jclouds
+
+python travis.py flink
+
+
+
